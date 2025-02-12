3515998f61836dbfc3d8557c3dfa64630bbcd15b
cyberllloner <cyberllloner@proton.me>
2025-02-12 18:54:37 +0330

    Add: base code for greeting

--- sudo-quotes.sh
+++ sudo-quotes.sh
@@ -4,6 +4,7 @@ BOLD="\033[1m"
 NOCOLOR="\033[0m"
 
 randomQuote=$(shuf -n 1 quotes.txt)
+randomGreeting=$(shuf -n 1 greetings.txt)
 
 quote=$(echo "$randomQuote" | cut -d '-' -f 1)
 source=$(echo "$randomQuote" | cut -d '-' -f 2)
