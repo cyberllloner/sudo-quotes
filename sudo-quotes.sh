d13161843811eba938890370c6d9db7750550b6e
cyberllloner <cyberllloner@proton.me>
2025-02-13 11:43:02 +0330

    Improve output formatting

--- sudo-quotes.sh
+++ sudo-quotes.sh
@@ -3,6 +3,7 @@
 BOLD="\033[1m"
 NOCOLOR="\033[0m"
 RED="\033[31m"
+WRAP_WIDTH=60
 
 if [[ ! -f quotes.txt ]]; then
   echo -e "\n${RED}${BOLD}Error:${NOCOLOR} quotes.txt not found!\n"
@@ -29,6 +30,9 @@ fi
 quote=$(echo "$randomQuote" | cut -d '-' -f 1)
 source=$(echo "$randomQuote" | cut -d '-' -f 2)
 
-echo -e "\n${BOLD}${quote}${NOCOLOR}\n"
-echo -e "— ${BOLD}${source}${NOCOLOR}\n"
+wrappedQuote=$(echo "$quote" | fold -s -w $WRAP_WIDTH)
+wrappedGreeting=$(echo "$source" | fold -s -w $WRAP_WIDTH)
+
+echo -e "\n${BOLD}${wrappedQuote}${NOCOLOR}\n"
+echo -e " — ${BOLD}${wrappedGreeting}${NOCOLOR}\n"
 echo -e "  "${randomGreeting}"\n"
